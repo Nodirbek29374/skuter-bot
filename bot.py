96ac89e383c4dcfba6337d2959314637cbe57656
Nodirbek29374 <fayziyevnodirbek917@gmail.com>
2026-04-28 00:38:40 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Bot ishlayapti ✅")
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.send_message(message.chat.id, f"Siz yozdingiz: {message.text}")
+
+bot.polling(none_stop=True)
