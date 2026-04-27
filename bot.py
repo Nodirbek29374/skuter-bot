7b97b027b3b9ae25b899ac273a468027fa1a17c2
Nodirbek29374 <fayziyevnodirbek917@gmail.com>
2026-04-28 02:05:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,30 @@
 import telebot
 import os
+import time
 from telebot import types
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# user ma'lumotlari
+PRICE_PER_MINUTE = 300  # 1 minut = 300 so'm
+
 users = {}
 
 def get_user(user_id):
     if user_id not in users:
         users[user_id] = {
             "balance": 0,
-            "riding": False
+            "riding": False,
+            "start_time": 0
         }
     return users[user_id]
 
-# MENU
 def main_menu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("🔋 Balance", "💳 Top Up")
     markup.add("🛴 Start Ride", "❌ End Ride")
     return markup
 
-# START
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(
@@ -32,43 +33,52 @@ def start(message):
         reply_markup=main_menu()
     )
 
-# BALANCE
 @bot.message_handler(func=lambda m: m.text == "🔋 Balance")
 def balance(message):
     user = get_user(message.chat.id)
-    bot.send_message(message.chat.id, f"Balansingiz: {user['balance']} so'm")
+    bot.send_message(message.chat.id, f"Balans: {user['balance']} so'm")
 
-# TOP UP
 @bot.message_handler(func=lambda m: m.text == "💳 Top Up")
 def topup(message):
     user = get_user(message.chat.id)
-    user['balance'] += 5000
+    user["balance"] += 5000
     bot.send_message(message.chat.id, "5000 so'm qo'shildi 💰")
 
-# START RIDE
 @bot.message_handler(func=lambda m: m.text == "🛴 Start Ride")
 def start_ride(message):
     user = get_user(message.chat.id)
 
-    if user["balance"] < 1000:
+    if user["balance"] < 300:
         bot.send_message(message.chat.id, "Balans yetarli emas ❌")
         return
 
     user["riding"] = True
-    bot.send_message(message.chat.id, "Skuter ochildi 🛴")
+    user["start_time"] = time.time()
 
-# END RIDE
+    bot.send_message(message.chat.id, "Skuter boshlandi 🛴")
+
 @bot.message_handler(func=lambda m: m.text == "❌ End Ride")
 def end_ride(message):
     user = get_user(message.chat.id)
 
     if not user["riding"]:
-        bot.send_message(message.chat.id, "Siz minmayapsiz ❌")
+        bot.send_message(message.chat.id, "Siz ride boshlamagansiz ❌")
         return
 
-    user["riding"] = False
-    user["balance"] -= 1000
+    end_time = time.time()
+    duration = int((end_time - user["start_time"]) / 60)
 
-    bot.send_message(message.chat.id, "Ride tugadi ✅ 1000 so'm yechildi")
+    if duration == 0:
+        duration = 1  # minimum 1 minut
+
+    cost = duration * PRICE_PER_MINUTE
+
+    user["balance"] -= cost
+    user["riding"] = False
+
+    bot.send_message(
+        message.chat.id,
+        f"Ride tugadi ✅\nVaqt: {duration} minut\nNarx: {cost} so'm"
+    )
 
 bot.infinity_polling()
