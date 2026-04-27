8e3fe24bce0dc438b80f2c24729cb91eecde98ec
Nodirbek29374 <fayziyevnodirbek917@gmail.com>
2026-04-28 01:38:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,28 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def start(message):
     bot.send_message(message.chat.id, "Bot ishlayapti ✅")
 
-@bot.message_handler(func=lambda m: True)
-def echo(message):
-    bot.send_message(message.chat.id, f"Siz yozdingiz: {message.text}")
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda m: True)
+def handle(message):
+    text = message.text
+
+    if text == "Balance":
+        bot.send_message(message.chat.id, "Sizning balans: 0 so'm 💰")
+
+    elif text == "Top Up":
+        bot.send_message(message.chat.id, "Pul qo‘shish uchun: Click / Payme link")
+
+    elif text == "Scooters Map":
+        bot.send_message(message.chat.id, "📍 Skuterlar xaritasi")
+
+    elif text == "Find Nearest":
+        bot.send_message(message.chat.id, "📍 Eng yaqin skuter qidirilmoqda...")
+
+    elif text == "End Ride":
+        bot.send_message(message.chat.id, "⛔ Ride tugatildi")
+
+    elif text == "Help":
+        bot.send_message(message.chat.id, "Yordam: tugmalardan foydalaning")
+
+    else:
+        bot.send_message(message.chat.id, "Noma'lum buyruq ❗")
